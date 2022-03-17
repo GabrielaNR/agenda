d85054ca245f69c109e025bf3328903cd501e09b
Gabriela <gabriela.trabback@gmail.com>
2022-03-17 11:17:42 -0300

    primeira versão

---
+++ Contato.java
@@ -0,0 +1,115 @@
+
+package com.mycompany.agendatelefonica;
+
+
+public class Contato {
+//Atributos
+    private int codigo;
+    private String nome;
+    private String endereço;
+    private String cidade;
+    private String estado;
+    private String telefone;
+    private String celular;
+    private String dataNasc;
+    private String email;
+    private String comentario;
+    
+//Métodos
+    public void incluir(){
+    
+}
+    public void salvar(){
+        
+}
+    public void excluir(){
+        
+}
+    public void sair(){
+        
+}
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+    
+
+
+
+}
